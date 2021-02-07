3c86e6ca0ddca34a8c670e8e380e3858725fa857
Joey Spencer <Joseph.Spencer99@gmail.com>
2021-02-07 20:55:08 +0000

    Reformat email

--- Client/Email.py
+++ Client/Email.py
@@ -3,47 +3,37 @@ import cv2
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
-from email.mime.base import MIMEBase
-#from email.MIMEimage import MIMEImage
-import email.mime
-import base64
 
 port = 465
 email = "JSTYP21@gmail.com"
 password = "Turing41"
-subject = "Subject: PiVision Security Alert \n\n"
-footer = "\n\nThis message was sent by your PiVision device."
-img = cv2.imread('image/4n.png')
+subject = "PiVision Security Alert"
+footer = "This message was sent by your PiVision device."
+
+# Convert image CV2 -> jpg bytes
+img = cv2.imread('image/1.png')
 encodedImage = cv2.imencode(".jpg", img)[1].tobytes()
-# jpg_as_text = base64.b64encode(encodedImage)
 
-
-
-
+# Mime settings, create message
 msg = MIMEMultipart()
-msg['From'] = email
-msg['To'] = "dragonslash42@gmail.com"
-msg['Subject'] = "PiVision"
-msg.attach(MIMEText('<b>Below is an image from your device</b><br><img src="cid:image1"><br>Sent from your PiVision device.', 'html'))
+msg["From"] = email
+msg["To"] = "dragonslash42@gmail.com"
+msg["Subject"] = subject
+msg.attach(MIMEText('<b>Below is an image from your device</b><br><img src="cid:image1"><br>' + footer, 'html'))
 
-
-fp = open('image/1.png', 'rb')
-img2 = fp.read()
-
-# print(encodedImage)
-
-
+# Add image to message
 image = MIMEImage(encodedImage)
 image.add_header('Content-ID', '<image1>')
 msg.attach(image)
 
 # Create SSL context
-context = ssl.create_default_context()
+sslContext = ssl.create_default_context()
 
-server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
+# Login to gmail
+server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=sslContext)
 server.login(email, password)
 
+# Send message
 text = msg.as_string()
-
 server.sendmail(email, "dragonslash42@gmail.com",
                 text)
